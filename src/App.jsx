a0c50a47a438a1f0d95e3363067aaf58151c2ece
hannanehhh16 <hannaneh.hatamy@gmail.com>
2025-05-30 01:17:11 +0330

    create base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import FooterComponent from './components/ui/Footer.Component';
+import NavbarComponent from './components/ui/navbar.component';
+import WrapperComponent from './components/ui/wrapper.component';
 
 function App() {
   return (
     <div className="App">
-
+      <NavbarComponent />
+      <WrapperComponent />
+      <FooterComponent />
     </div>
   );
 }
