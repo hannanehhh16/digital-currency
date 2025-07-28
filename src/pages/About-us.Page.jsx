36ac3e19cb1338d85cf9ff0102bbd67ba2340641
hannanehhh16 <hannaneh.hatamy@gmail.com>
2025-07-29 01:14:28 +0330

    made about-us page

--- src/pages/About-us.Page.jsx
+++ src/pages/About-us.Page.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import aboutImg from "../assets/images/about.png"
 export default function AboutUsPage() {
     return (
-        <div>AboutUsPage</div>
+        <div className='m-4'>
+            <div className="p-3 bg-info rounded-3">
+                <h3>About Us</h3>
+            </div>
+            <div className="d-md-flex d-flex-column alight-item-center justify-content-center">
+                <div className="d-flex alight-item-center justify-content-center m-4">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium accusantium ipsam rerum nulla, eum sit unde incidunt architecto dicta veniam, explicabo consequuntur modi est enim labore id corporis ducimus!</p>
+                </div>
+                <div className="d-flex alight-item-center justify-content-center">
+                    <img className="about-img" src={aboutImg} alt="about" />
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/Faq.Page.jsx
+++ src/pages/Faq.Page.jsx
@@ -4,7 +4,7 @@ export default function FaqPage() {
     return (
         <div>
             <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
+                <div class="accordion-item ">
                     <h2 class="accordion-header">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             QUASTION 1
