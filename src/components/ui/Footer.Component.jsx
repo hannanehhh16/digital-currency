33b52922ce07ee6fa805da9085ee6418f845fd6c
hannanehhh16 <hannaneh.hatamy@gmail.com>
2025-08-02 18:21:37 +0330

    finished :)

--- src/components/ui/Footer.Component.jsx
+++ src/components/ui/Footer.Component.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function FooterComponent() {
   return (
     <footer className="footer">
-      <span>Faradars Crypto Tracking.</span>
+      <span>Crypto Tracking.</span>
       <span className="d-none d-md-inline ms-2">All rights reserved </span>
     </footer>
   )
